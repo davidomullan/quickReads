0213f409d197f937b9f998f88c065e81937a986d
David OMullan <davidomullan@gmail.com>
2023-08-10 22:26:34 +0000

    Create basic User model and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root              "static_pages#home"
+  root                "static_pages#home"
   
-  get '/home',  to: "static_pages#home"
-  get '/help',  to: "static_pages#help"
-  get '/about', to: "static_pages#about"
+  get '/home',    to: "static_pages#home"
+  get '/help',    to: "static_pages#help"
+  get '/about',   to: "static_pages#about"
+  get '/signup',  to: "users#new"
   # get '/discover', to: "stories#show"
 end
