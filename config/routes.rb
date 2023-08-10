8d2701d9cf9030512e68a19b70ed05af5789d073
David OMullan <davidomullan@gmail.com>
2023-08-10 15:46:02 +0000

    Create Header and Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root              "static_pages#home"
   
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/home',  to: "static_pages#home"
+  get '/help',  to: "static_pages#help"
+  get '/about', to: "static_pages#about"
+  # get '/discover', to: "stories#show"
 end
