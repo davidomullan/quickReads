1d735940b2f7804bd2532129b3ad43f86837d00e
David OMullan <davidomullan@gmail.com>
2023-08-11 21:27:28 +0000

    Update heroku production settings

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# Use RCT for integration testing
+gem 'rails-controller-testing', '~> 1.0', '>= 1.0.5'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_response :unprocessable_entity
-    assert_template 'users/new'
+    assert_template 'users/new' # Requires gem 'rails-controller-testing'
+  end
+  
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show' # Requires gem 'rails-controller-testing'
   end
 end
